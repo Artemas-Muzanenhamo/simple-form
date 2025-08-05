cbf8c740340efb6bc262c3fb51405e310147f232
Artemas-Muzanenhamo <29547780+Artemas-Muzanenhamo@users.noreply.github.com>
2025-08-05 22:34:32 +0100

    add a simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'simple-form';
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      age: ['', [Validators.required, Validators.min(1)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Form Submitted:', this.form.value);
+      alert('Form submitted successfully!');
+    } else {
+      alert('Please fill out all fields correctly.');
+    }
+
+  }
 }
